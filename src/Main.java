d5bc915c593d5cc80015ae4a1317f8a5d9633845
baisal02 <altynbekuulubajsal@gmail.com>
2025-03-13 13:07:15 +0600

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
-    public static void main(String[] args) 
-       
-   
-        
-        
-        
-        System.out.println("newbranch");
+    public static void main(String[] args){
+
+        System.out.println("fixed");
      
     }
 }
